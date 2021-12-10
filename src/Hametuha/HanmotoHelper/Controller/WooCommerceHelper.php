6da4e1b82fb798f79a5a388929359bd452643ab4
fumikito <guy@hametuha.com>
2021-12-10 18:20:27 +0900

    Add WooCommerce bootstrap.

--- src/Hametuha/HanmotoHelper/Controller/WooCommerceHelper.php
+++ src/Hametuha/HanmotoHelper/Controller/WooCommerceHelper.php
@@ -12,4 +12,26 @@ use Hametuha\HanmotoHelper\Pattern\Singleton;
  */
 class WooCommerceHelper extends Singleton {
 
+	/**
+	 * @inheritDoc
+	 */
+	protected function init() {
+		// Register hooks.
+		add_filter( 'woocommerce_product_data_tabs', [ $this, 'product_tab' ] );
+	}
+
+	/**
+	 * Get product tabs.
+	 *
+	 * @param array $tabs
+	 * @return array
+	 */
+	public function product_tab( $tabs ) {
+		$tabs['bibliography'] = [
+			'label'    => __( '書誌情報', 'hanmoto' ),
+			'target'   => 'bibliography_product_data',
+			'priority' => 30,
+		];
+		return $tabs;
+	}
 }
